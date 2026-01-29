42a29cbfadd8d9952046809095f0c77fc793dfe6
Rockfull <orianaroru@gmail.com>
2026-01-29 12:47:12 -0400

    chore: enable remix v3 future flags to silence build warnings

--- vite.config.js
+++ vite.config.js
@@ -28,6 +28,13 @@ export default defineConfig({
 
     remix({
       presets: [vercelPreset()],
+      future: {
+        v3_fetcherPersist: true,
+        v3_relativeSplatPath: true,
+        v3_throwAbortReason: true,
+        v3_singleFetch: true,
+        v3_lazyRouteDiscovery: true,
+      },
     }),
     jsconfigPaths(),
   ],
